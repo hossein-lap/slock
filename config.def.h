1f2308b3d922f46658a44d965b1178bad433e0ee
hossein <hosteam01@gmail.com>
2023-11-19 22:16:55 +0330

    default colors as modules (.h files)

--- config.def.h
+++ config.def.h
@@ -1,10 +1,4 @@
-static const char *colorname[NUMCOLS] = {
-	[BACKGROUND] =   "black",     /* after initialization */
-	[INIT] =   "#2d2d2d",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "#ff7700",         /* CapsLock on */
-};
+#include "colors/default.h"
 
 /*
  * Xresources preferences to load at startup
